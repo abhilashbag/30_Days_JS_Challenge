e0fe7d3d410442bc0770ea75188a400e26e5c33c
abhilashbag <abhilashbag03@gmail.com>
2024-07-14 08:43:43 +0530

    Initial commit

---
+++ Operators/Day1.js
@@ -0,0 +1,37 @@
+//Task 1 
+var number = 20;
+console.log(number)
+
+
+//Task 2
+let string = "Anna";
+console.log(string)
+
+
+//Task 3
+const one = true;
+console.log(one);
+
+// Task 4
+let num = 10;
+let str = 'Hello'
+let yes = Boolean;
+let Do = {
+    name: "abhi",
+}
+let heros = ["superman","spiderman"]
+
+console.log(typeof num)
+console.log(typeof str)
+console.log(typeof yes)
+console.logt(typeof Do)
+console.log(typeof heros)
+
+// Task 5
+let myname = "Shahid";
+myname = "Abhi"
+
+
+//Task 6
+const game = "PUBG"
+game= "LUDO";
